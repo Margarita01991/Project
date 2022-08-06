346064e46b1f79f8e534626bd12581144f207053
Margarita <margarita27011991@gmail.com>
2022-08-06 17:36:22 +0300

    add random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
 ﻿//Console.WriteLine ("Введите целое число");
-int numberA = 31;
-int numberB = 415; 
+int numberA = new Random().Next(1,10); // 1 2 3 4 5 6 7 8 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10); // 1 2 3 ... 9
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.Write(result);
