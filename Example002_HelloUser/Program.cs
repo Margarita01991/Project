7b92794628e528b3f95d3d81370265a4855ab428
Margarita <109097741+Margarita01991@users.noreply.github.com>
2022-08-09 11:10:02 +0300

    add comment for 2 line

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.WriteLine ("Ведите ваше имя");
-string username = Console.ReadLine();
+string? username = Console.ReadLine();//? для устранения ошибки значения NULL в ()
 Console.Write ("Привет, ");
 Console.Write (username);
